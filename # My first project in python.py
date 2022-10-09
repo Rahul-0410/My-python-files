b3af6e2d1bbb901966df1451583f7636ca8b3526
Rahul-0410 <115411672+Rahul-0410@users.noreply.github.com>
2022-10-09 20:52:28 +0530

    Add files via upload

---
+++ # My first project in python.py
@@ -0,0 +1,22 @@
+# Rahul
+print("name")
+#displaying texts
+print('rahul is sleeping')
+print("rahul is sleeping")
+# about capybara
+print("capybara is world's largest rodent")
+print("capybara likes to live in group")
+print("capybara can swiprint")
+print("rahul is good.\n he is happy")
+print("'here is a double quote in a single quote'" +  '"here is a single quote in a double quote"')
+# i am putting a slash slash so that the slash appears in string
+print(" hello world \\news")
+# witing a poem from alison 
+print(" There once was a movie star icon")
+print("who preferred to sleep with the light on")
+print("They learned how to code")
+print("a device that sure glowed")
+print("and lit up the night using python!")
+# using now string and variables 
+name = input("What's your name?")
+print(name)
