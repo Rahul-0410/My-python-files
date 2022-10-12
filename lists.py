a064cfaf4d3687598da3a2faa2ee4de1269e6de4
Rahul <115411672+Rahul-0410@users.noreply.github.com>
2022-10-12 09:45:30 +0530

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,22 @@
+name1 = "name first"
+name2 = "name second"
+name3 = "name third"
+
+name = ["name1", "name2", "name3"]
+
+print(name[2])
+numbers = [1, 2, 3]
+numbers.append(4)
+print(numbers)
+numbers.pop(1)
+print(numbers)
+numbers.insert(1,5)
+
+print(numbers)
+del numbers[2]
+print(numbers)
+number = [1,2,3,4,5,6,7,8,9]
+del number[0:4]
+print(number)
+number.clear()
+print(number)
---
+++ numers.py
@@ -0,0 +1,11 @@
+area = 0
+height = 20
+width = 10
+area = height * width
+print(" The area of the rectangle is %.2f" %area)
+print("Here are the first three numbers !" + \
+    "The first is {0:d}  The second is {1:4d}  The third is {2:d}".format(7,8,9))
+salary = input("please enter your salary")
+bonus = input("please input your salary")
+paycheck = float(salary) + float(bonus)
+print(paycheck)
